10214fea4d8e2508091460002733ad4c1831b59b
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2021-01-13 15:26:33 +0800

    allow user to choose which csv file to be read

--- Core/FileReader/CsvReader.cs
+++ Core/FileReader/CsvReader.cs
@@ -8,10 +8,39 @@ namespace Core.FileReader
 {
     public class CsvReader
     {
-        private static string csvLocation = AppSettings.hdaTagsCSVLocation;
+        private static Dictionary<int, string> CSVLocationMap = new Dictionary<int, string>()
+        {
+            {1,  AppSettings.hdaTagsCSVLocation1},
+            {2,  AppSettings.hdaTagsCSVLocation2},
+            {3,  AppSettings.hdaTagsCSVLocation3},
+            {4,  AppSettings.hdaTagsCSVLocation4},
+            {5,  AppSettings.hdaTagsCSVLocation5},
+            {6,  AppSettings.hdaTagsCSVLocation6},
+        };
+
+        private static string getUserChoiceCsv()
+        {
+            string choice = "";
+            int choiceInt;
+            Console.WriteLine("Below is the list of hda tag csv files available... ");
+            foreach (KeyValuePair<int, string> kvp in CSVLocationMap)
+            {
+                Console.WriteLine("Choice {0}: {1}", kvp.Key, kvp.Value);
+            }
+
+            while (!int.TryParse(choice, out choiceInt) || choiceInt < 1 || choiceInt > 6)
+            {
+                // keep asking for user input if input is invalid
+                // for e.g. not an integer, integer not from 1 to 6
+                Console.Write("Please choose the csv file to read (enter a valid number, from 1 to 6): ");
+                choice = Console.ReadLine();
+            }
+
+            return CSVLocationMap[choiceInt];          
+        }
         public static List<string> readCsv()
         {
-            using var streamReader = File.OpenText(csvLocation);
+            using var streamReader = File.OpenText(getUserChoiceCsv());
             using var csvReader = new CsvHelper.CsvReader(streamReader, CultureInfo.CurrentCulture);
             csvReader.Configuration.HasHeaderRecord = true;
             csvReader.Configuration.ShouldSkipRecord = row => row[0].Contains("HDA_TAGS");
